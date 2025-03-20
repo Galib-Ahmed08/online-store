4ff032c444240f5bab4cb5f152583ebdabaa17f4
Galib-Ahmed08 <galibahmedofficial@gmail.com>
2025-03-21 03:40:05 +0600

    Update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function showMessage() {
-    alert("Welcome to our store!");
+    alert("Welcome to our store! Enjoy shopping.");
+}
+
+function addToCart(productName, price) {
+    alert(productName + " has been added to your cart for $" + price);
 }
